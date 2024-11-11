c7cf43fb9d14a17aba7aaeb9c648a4aa51567d77
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-11-11 16:35:06 +0200

    export Tree.js

--- Tree.js
+++ Tree.js
@@ -176,13 +176,4 @@ class Tree {
     }
 }
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-
-tree.insert(100)
-tree.insert(101)
-tree.insert(102)
-tree.insert(103)
-prettyPrint(tree.root);
-// tree.inOrder(78);
-tree.rebalance();
-prettyPrint(tree.root);
+export default Tree;
